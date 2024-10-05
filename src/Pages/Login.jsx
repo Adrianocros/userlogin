482fbae6827bee87ce31324ea9328f7ffc122053
Adrianocros <adrianobianchi@outlook.com>
2024-10-04 23:40:36 -0300

    Estilizando tela de login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,18 @@
 function Login(){
     return(
     <div className="container-login">
-        <h1>Login</h1>
+        <form  className="form-login">
+            <h1 className="tituloLogin">{'Acesso ao cadastro'}</h1>
+            <label htmlFor="">Usuario</label>
+            <input type="text" name="userName"  placeholder="Usuario Protheus"/>
+            <label htmlFor="">Senha</label>
+            <input type="password" name="password" className="placeSenha" placeholder="Senha Protheus"/>
+            <div className="buttons">
+                <button className="button-confirmar">Acessar</button>
+              
+
+            </div>
+        </form>
     </div>
     )
 }
