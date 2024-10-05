5cffe0d41b808bd68c7852db5240648a79c1a932
Adrianocros <adrianobianchi@outlook.com>
2024-10-04 22:39:05 -0300

    Estilizando o Header e Footer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+
 function Header(){
     return(
         <div className="container-header">
-            <h1>Header</h1>
+            <h1 className="logo">{'Cadastro de Usuario Protheus'}</h1>
+            <nav className="nav-bar">
+            {
+                localStorage.getItem('@Usuario_Protheus/displayname') === null
+                ? 
+                <>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/login">Login</NavLink>
+                </>
+                :
+                <>
+                <NavLink to="/">Users</NavLink>
+                <NavLink to="/logout">Logout</NavLink>
+                </>
+            }
+            </nav>
         </div>
+
     )
 }
 export default Header;
